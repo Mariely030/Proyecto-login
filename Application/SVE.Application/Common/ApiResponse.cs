f4c4489d86205c3500f84cd1512c4102ed83bc70
Mariely030 <rosse.mmp@gmail.com>
2026-04-03 03:30:16 -0400

    Delete Application directory

--- Application/SVE.Application/Common/ApiResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace SVE.Application.Common
-{
-    public class ApiResponse<T>
-    {
-        public bool Success { get; set; }
-        public string? Message { get; set; }
-        public T? Data { get; set; }
-    }
-}
--- Application/SVE.Application/Dtos/Network/NetworkProvider/DisableNetworkProviderDtos.cs
+++
@@ -1,9 +0,0 @@
-namespace SVE.Application.Dtos.Network.NetworkProvider
-{
-    public record DisableNetworkProviderDtos
-    {
-        public int Id { get; set; }
-        public DateTime? UpdateAt { get; set; }
-    }
-
-}
